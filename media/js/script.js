f8a5b50fea4b85971031644938fb253970e54e6f
JCeb <julian@pikhub.com>
2012-06-24 15:58:57 -0500

    .gitignore added, header modified, backbone handlebars underscore added

--- media/js/script.js
+++ media/js/script.js
@@ -2,9 +2,11 @@
 
 */
 
-
-
-
-
-
-
+// Listeners
+$('#menu').hover(function() {
+  var self = $(this);
+  self.children('.dropdown-menu').show();
+}, function() {
+  var self = $(this);
+  self.children('.dropdown-menu').hide();
+});
